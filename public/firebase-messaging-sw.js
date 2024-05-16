1311528fd50878b60d36c9ac3917089354158d44
Jacky <87586598+jcheng1022@users.noreply.github.com>
2024-05-16 14:56:30 -0400

    sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -30,26 +30,5 @@ messaging.onBackgroundMessage(function(payload) {
     self.registration.showNotification(notificationTitle,
         notificationOptions);
 
-    self.addEventListener('notificationclick', (event) => {
-        event.notification.close(); // CLosing the notification when clicked
-        const urlToOpen = event?.notification?.data?.url || 'https://www.test.com/';
-        // Open the URL in the default browser.
-        event.waitUntil(
-            clients.matchAll({
-                type: 'window',
-            })
-                .then((windowClients) => {
-                    // Check if there is already a window/tab open with the target URL
-                    for (const client of windowClients) {
-                        if (client.url === urlToOpen && 'focus' in client) {
-                            return client.focus();
-                        }
-                    }
-                    // If not, open a new window/tab with the target URL
-                    if (clients.openWindow) {
-                        return clients.openWindow(urlToOpen);
-                    }
-                })
-        );
-    });
+
 });
