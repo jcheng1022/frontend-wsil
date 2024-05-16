aaeaa6aadedd78d061523e5d257576e6fadfd104
Jacky <87586598+jcheng1022@users.noreply.github.com>
2024-05-16 14:08:09 -0400

    try

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
-import {firebaseConfig} from "@/lib/firebase/firebase";
+
+importScripts(
+    'https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js'
+);
+importScripts(
+    'https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js'
+);
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBcIbcre15_W5qlquK6ceuW0n0qLoqmCto",
+    authDomain: "wsil-fb.firebaseapp.com",
+    projectId: "wsil-fb",
+    storageBucket: "wsil-fb.appspot.com",
+    messagingSenderId: "519965076207",
+    appId: "1:519965076207:web:9ccb5288624e7dd84b891a",
+    measurementId: "G-RFSH8MTEB4"
+});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
